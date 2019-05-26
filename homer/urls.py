ab154c20ada68998d2b1e5c6e36b7e4026faa71b
Titi Wangsa Damhore <ttwd80@gmail.com>
2019-05-27 05:26:08 +1000

    main app okay

--- homer/urls.py
+++ homer/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import include, path
 
 urlpatterns = [
-    path('taketest/', include('taketest.urls')),
+    path('', lambda r: HttpResponseRedirect('main/')),
+    path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
--- taketest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaketestConfig(AppConfig):
-    name = 'taketest'
