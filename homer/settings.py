e5d27ddb31adcdadf0e894cf3d810936195577a4
Titi Wangsa Damhore <ttwd80@gmail.com>
2019-05-27 06:22:25 +1000

    db to use postgresql

--- homer/settings.py
+++ homer/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homer',
+        'USER': os.env('DB_USERNAME'),
+        'PASSWORD': os.env('DB_PASSWORD'),
+        'HOST': os.env('DB_HOST'),
+        'PORT': os.env('DB_PORT')
     }
 }
 
