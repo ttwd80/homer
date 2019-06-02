f2609dfca2851c26bdeef76ae22238e56f017123
Titi Wangsa Damhore <ttwd80@gmail.com>
2019-06-02 14:47:04 +1000

    java 11

---
+++ src/main/java/com/github/ttwd80/homer/HomerApplication.java
@@ -0,0 +1,13 @@
+package com.github.ttwd80.homer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomerApplication.class, args);
+	}
+
+}
