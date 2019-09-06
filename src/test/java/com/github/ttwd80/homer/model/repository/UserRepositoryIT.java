96bad5f70e44259de200c4cbf9fed9c826a89e28
Titi Wangsa Damhore <ttwd80@gmail.com>
2019-09-07 07:57:39 +1000

    integration test user and role

--- src/test/java/com/github/ttwd80/homer/model/repository/UserRepositoryIT.java
+++ src/test/java/com/github/ttwd80/homer/model/repository/UserRepositoryIT.java
@@ -2,6 +2,7 @@ package com.github.ttwd80.homer.model.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.github.ttwd80.homer.model.entity.Role;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,30 +12,69 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import com.github.ttwd80.homer.model.entity.User;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
+
 @Transactional
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class UserRepositoryIT {
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @Autowired
+    private RoleRepository roleRepository;
 
-	@Test
-	void testNotExist() {
-		assertThat(userRepository.findById("random-id")).isEmpty();
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+        roleRepository.deleteAll();
+        userRepository.deleteAll();
+    }
 
-	@Test
-	void testInsert() {
-		assertThat(userRepository.findById("random-id")).isEmpty();
-		var user = new User();
-		user.setUsername("random-id");
-		user.setPassword("random-password");
-		userRepository.save(user);
-		assertThat(userRepository.findById("random-id").get().getPassword()).isEqualTo("random-password");
-	}
+    @Test
+    void testNotExist() {
+        assertThat(userRepository.findById("random-id")).isEmpty();
+    }
+
+    @Test
+    void testInsert() {
+        assertThat(userRepository.findById("random-id")).isEmpty();
+        var user = new User();
+        user.setUsername("random-id");
+        user.setPassword("random-password");
+        userRepository.save(user);
+        assertThat(userRepository.findById("random-id").get().getPassword()).isEqualTo("random-password");
+    }
+
+    @Test
+    void testInsertMultipleRoles() {
+        {
+            var role = new Role();
+            role.setRolename("ROLE_ADMIN");
+            roleRepository.save(role);
+        }
+        {
+            var role = new Role();
+            role.setRolename("ROLE_USER");
+            roleRepository.save(role);
+        }
+        assertThat(userRepository.findById("random-id")).isEmpty();
+        var user = new User();
+        user.setUsername("random-id");
+        user.setPassword("random-password");
+        var roles = new HashSet<Role>();
+        {
+            var role = new Role();
+            role.setRolename("ROLE_ADMIN");
+            roles.add(role);
+        }
+        {
+            var role = new Role();
+            role.setRolename("ROLE_USER");
+            roles.add(role);
+        }
+        user.setRoles(roles);
+        userRepository.save(user);
+        assertThat(userRepository.findById("random-id").get().getRoles().size()).isEqualTo(2);
+    }
 
 }
